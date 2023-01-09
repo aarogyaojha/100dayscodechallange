3b3f1b193255884c8d1b454db066fa5a1012cf68
Aarogya Ojha <95065228+aarogyaojha@users.noreply.github.com>
2023-01-09 22:22:02 +0530

    Create amsnum.c

---
+++ amsnum.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int n,temp,r,sum=0;
+    printf("enter the number");
+    scanf("%d",&n);
+    temp=n;
+    while(n>0)
+    {
+        r=n%10;
+        sum=sum+(r*r*r);
+        n=n/10;
+    }
+    if (temp==sum)
+    {
+        printf("amstrong");
+    }
+    else
+    {
+        printf("not amstrong");
+    }
+    return 0;
+}
